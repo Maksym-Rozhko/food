14ae77b16da8643c18c21cf0506598efc17df2f6
Maksym <maksymRozhko7@gmail.com>
2022-02-23 02:23:10 +0200

    use class for render cards

--- js/script.js
+++ js/script.js
@@ -129,3 +129,122 @@ document.addEventListener('keydown', e => {
 const modalTimerId = setTimeout(showModal, 10000);
 
 window.addEventListener('scroll', showModalByScroll);
+
+// Use class for cards
+
+class MenuCard {
+    constructor (src, alt, title, text, price, parentSelector) {
+        this.src = src;
+        this.alt = alt;
+        this.title = title;
+        this.text = text;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 29;
+        this.changeToUAH();
+        this.parent.innerHTML = '';
+    };
+
+    changeToUAH() {
+        this.price = this.price * this.transfer;
+    };
+
+    render() {
+
+        const card = document.createElement('div');
+
+        card.innerHTML = `
+            <div class="menu__item">
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.text}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `;
+
+        this.parent.append(card);
+    };
+};
+
+const card1 = new MenuCard(
+    'img/tabs/vegy.jpg', 
+    'vegy', 
+    'Меню "Фитнес"', 
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 
+    10, 
+    '.menu__field .container'
+);
+
+const card2 = new MenuCard(
+    'img/tabs/elite.jpg', 
+    'elite', 
+    'Меню "Премиум"', 
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 
+    25, 
+    '.menu__field .container'
+);
+
+const card3 = new MenuCard(
+    'img/tabs/post.jpg', 
+    'post', 
+    'Меню "Постное"', 
+    'В меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 
+    19, 
+    '.menu__field .container'
+);
+
+card1.render();
+card2.render();
+card3.render();
+
+
+
+
+// functions constructor
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}!`);
+//     };
+// };
+
+// class User {
+//     constructor (name, id) {
+//         this.name = name;
+//         this.id = id;
+//         this.human = true;
+//     };
+//     hello() {
+//         console.log(`Hello ${this.name}!`);
+//     };
+//     exit() {
+//         console.log(`Пользователь ${this.name} покинул чат`);
+//     };
+// };
+
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name} покинул чат`);
+// };
+
+// let newUser1 = new User('David', 28);
+// let newUser2 = new User('Alex', 20);
+
+// // newUser2.status = 'merried';
+
+// newUser1.hello();
+// newUser2.hello();
+
+// setTimeout(() => {
+//     newUser1.exit();
+//     newUser2.exit();
+// }, 2000);
+
+// console.log(newUser1);
+// console.log(newUser2);
