e7abb513a47f7a6fafebe55a762ae6d4c6ec2855
Maksym <maksymRozhko7@gmail.com>
2022-03-01 18:19:48 +0200

    update send form, fetch

--- js/script.js
+++ js/script.js
@@ -233,35 +233,30 @@ function postData(form) {
             display: block;
             margin: 0 auto;
         `;
-        // form.append(statusMessage);
         form.insertAdjacentElement('afterend', statusMessage);
-
-        const request = new XMLHttpRequest();
-
-        request.open('POST', 'server.php');
-        // request.setRequestHeader('Content-type', 'application/json');
 
         const formData = new FormData(form);
 
-        // convert into json, uncomment setRequestHeader
-        // const object = {};
-        // formData.forEach(function(value, key) {
-        //     object[key] = value;
-        // });
-
-        // const json = JSON.stringify(object);
-        // request.send(json);
-
-        request.send(formData);
-
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                showThanksModal(message.success);
-                form.reset();
-                statusMessage.remove();
-            } else {
-                showThanksModal(message.failure);
-            };
+        const object = {};
+        formData.forEach(function(value, key) {
+            object[key] = value;
+        });
+        
+        fetch('server.php', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        }).then(data => data.text())
+        .then(data => {
+            console.log(data);
+            showThanksModal(message.success);
+            statusMessage.remove();
+        }).catch(() => {
+            showThanksModal(message.failure);
+        }).finally(() => {
+            form.reset();
         });
     });
 };
